9404dbc1f8558234e08b072370dd901b74884595
Ryusei7070 <renshima7070@gmail.com>
2021-02-01 21:22:48 +0900

    2/01 Ryusei login first commit

---
+++ iProjectTeacher/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  iProjectTeacher
+//
+//  Created by Ryusei Hiraoka on 2021/01/31.
+//  Copyright © 2021 Tanaka_Yoshihisa_4413. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

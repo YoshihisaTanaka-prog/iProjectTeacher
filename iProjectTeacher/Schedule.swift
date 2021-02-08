8e5918a860591a6dd98dbdbaf9a9bd91bf80797c
Ryusei7070 <renshima7070@gmail.com>
2021-02-08 22:33:22 +0900

    2/8 Ryusei

---
+++ iProjectTeacher/Schedule.swift
@@ -0,0 +1,32 @@
+//
+//  Schedule.swift
+//  iProjectTeacher
+//
+//  Created by Ryusei Hiraoka on 2021/02/08.
+//  Copyright © 2021 Tanaka_Yoshihisa_4413. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import NCMB
+
+class Schedule: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

a7fbba47d5cdf0e6a325ddafd43a2d56644c7f3b
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2021-02-11 18:35:55 +0900

    02/11 Yoshihisa add student infomation commit

---
+++ iProjectTeacher/StudentInfo/StudentDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StudentDetailViewController.swift
+//  iProjectTeacher
+//
+//  Created by 田中義久 on 2021/02/11.
+//  Copyright © 2021 Tanaka_Yoshihisa_4413. All rights reserved.
+//
+
+import UIKit
+
+class StudentDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ iProjectTeacher/StudentInfo/StudentInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentInfoTableViewCell.swift
+//  iProjectTeacher
+//
+//  Created by 田中義久 on 2021/02/11.
+//  Copyright © 2021 Tanaka_Yoshihisa_4413. All rights reserved.
+//
+
+import UIKit
+
+class StudentInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

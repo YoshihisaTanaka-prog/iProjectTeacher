eeb15f85bd0defafd742c6246767d96a4be6f3a0
Ryusei7070 <renshima7070@gmail.com>
2021-02-15 20:16:02 +0900

    2/15 Ryusei Calendar

--- iProjectTeacher/Calendar&Schedule/CalendarViewController.swift
+++ iProjectTeacher/Calendar&Schedule/CalendarViewController.swift
@@ -12,127 +12,132 @@ import CalculateCalendarLogic
 import RealmSwift
 
 class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
-     //スケジュール内容
+    //スケジュール内容
     @IBOutlet var labelDate: UILabel!
-        //「主なスケジュール」の表示
+    //「主なスケジュール」の表示
     @IBOutlet var labelTitle: UILabel!
-        //カレンダー部分
-   
-        //日付の表示
+    //カレンダー部分
+    
+    //日付の表示
     @IBOutlet var  Date: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @objc func onClick(){ let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    let SecondController = storyboard.instantiateViewController(withIdentifier: "Insert")
+        let SecondController = storyboard.instantiateViewController(withIdentifier: "Insert")
         present(SecondController, animated: true, completion: nil)
     }
     
     fileprivate let gregorian: Calendar = Calendar(identifier: .gregorian)
-       fileprivate lazy var dateFormatter: DateFormatter = {
-           let formatter = DateFormatter()
-           formatter.dateFormat = "yyyy-MM-dd"
-           return formatter
-       }()
-
-       // 祝日判定を行い結果を返すメソッド
-       func judgeHoliday(_ date : Date) -> Bool {
-           //祝日判定用のカレンダークラスのインスタンス
-           let tmpCalendar = Calendar(identifier: .gregorian)
-
-           // 祝日判定を行う日にちの年、月、日を取得
-           let year = tmpCalendar.component(.year, from: date)
-           let month = tmpCalendar.component(.month, from: date)
-           let day = tmpCalendar.component(.day, from: date)
-
-           let holiday = CalculateCalendarLogic()
-
-           return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
-       }
-
-       // date型 -> 年月日をIntで取得
-       func getDay(_ date:Date) -> (Int,Int,Int){
-           let tmpCalendar = Calendar(identifier: .gregorian)
-           let year = tmpCalendar.component(.year, from: date)
-           let month = tmpCalendar.component(.month, from: date)
-           let day = tmpCalendar.component(.day, from: date)
-           return (year,month,day)
-       }
-
-       //曜日判定
-       func getWeekIdx(_ date: Date) -> Int{
-           let tmpCalendar = Calendar(identifier: .gregorian)
-           return tmpCalendar.component(.weekday, from: date)
-       }
-
-       // 土日や祝日の日の文字色を変える
-       func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
-           //祝日判定をする
-           if self.judgeHoliday(date){
-               return UIColor.red
-           }
-
-           //土日の判定
-           let weekday = self.getWeekIdx(date)
-           if weekday == 1 {
-               return UIColor.red
-           }
-           else if weekday == 7 {
-               return UIColor.blue
-           }
-
-           return nil
-       }
-    //カレンダー処理(スケジュール表示処理)
-        func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition){
-
-            //予定がある場合、スケジュールをDBから取得・表示する。
-            //無い場合、「スケジュールはありません」と表示。
-            labelDate.text = "スケジュールはありません"
-            labelDate.textColor = .lightGray
-            view.addSubview(labelDate)
-
-            let tmpDate = Calendar(identifier: .gregorian)
-            let year = tmpDate.component(.year, from: date)
-            let month = tmpDate.component(.month, from: date)
-            let day = tmpDate.component(.day, from: date)
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
     
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy/MM/dd"
-            let da = formatter.string(from: date)
-            Date.text = da
-            view.addSubview(Date)
-            
-            
-
-            //スケジュール取得
+    // 祝日判定を行い結果を返すメソッド
+    func judgeHoliday(_ date : Date) -> Bool {
+        //祝日判定用のカレンダークラスのインスタンス
+        let tmpCalendar = Calendar(identifier: .gregorian)
         
-            let realm = try! Realm()
-            var result = realm.objects(Event.self)
-            result = result.filter("date = '\(da)'")
-            print(result)
-            for ev in result {
-                if ev.date == da {
-                    labelDate.text = ev.event
-                    labelDate.textColor = .black
-                    view.addSubview(labelDate)
-                }
-            }
-            
-            func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-                if result != nil {
-                    return 1
-                }else{
-                    return 0
-                }
-            }
-            
-            }
-
-            
-         
-            
+        // 祝日判定を行う日にちの年、月、日を取得
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+        
+        let holiday = CalculateCalendarLogic()
+        
+        return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
+    }
+    
+    // date型 -> 年月日をIntで取得
+    func getDay(_ date:Date) -> (Int,Int,Int){
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        let year = tmpCalendar.component(.year, from: date)
+        let month = tmpCalendar.component(.month, from: date)
+        let day = tmpCalendar.component(.day, from: date)
+        return (year,month,day)
+    }
+    
+    //曜日判定
+    func getWeekIdx(_ date: Date) -> Int{
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        return tmpCalendar.component(.weekday, from: date)
+    }
+    
+    // 土日や祝日の日の文字色を変える
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        //祝日判定をする
+        if self.judgeHoliday(date){
+            return UIColor.red
         }
+        
+        //土日の判定
+        let weekday = self.getWeekIdx(date)
+        if weekday == 1 {
+            return UIColor.red
+        }
+        else if weekday == 7 {
+            return UIColor.blue
+        }
+        
+        return nil
+    }
+    //カレンダー処理(スケジュール表示処理)
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition){
+        
+        //予定がある場合、スケジュールをDBから取得・表示する。
+        //無い場合、「スケジュールはありません」と表示。
+        labelDate.text = "スケジュールはありません"
+        labelDate.textColor = .lightGray
+        view.addSubview(labelDate)
+        
+        let tmpDate = Calendar(identifier: .gregorian)
+        let year = tmpDate.component(.year, from: date)
+        let month = tmpDate.component(.month, from: date)
+        let day = tmpDate.component(.day, from: date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let da = formatter.string(from: date)
+        Date.text = da
+        view.addSubview(Date)
+        
+        
+        
+        //スケジュール取得
+        
+        let realm = try! Realm()
+        var result = realm.objects(Event.self)
+        result = result.filter("date = '\(da)'")
+        print(result)
+        for ev in result {
+            if ev.date == da {
+                labelDate.text = ev.event
+                labelDate.textColor = .black
+                view.addSubview(labelDate)
+            }
+        }
+        
+        
+    }
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let da = formatter.string(from: date)
+        let realm = try! Realm()
+        var result = realm.objects(Event.self)
+        result = result.filter("date = '\(da)'")
+        if result.count != 0 {
+            return 1
+        }else{
+            return 0
+        }
+    }
+    
+    
+    
+}
 
 

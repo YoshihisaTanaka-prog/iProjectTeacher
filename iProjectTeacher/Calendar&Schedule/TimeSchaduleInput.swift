d04704a1703d32aa741001c71082afdcb862d368
knakamura17 <nakamura.kaori17@gmail.com>
2021-06-04 10:15:43 +0900

    空欄_中村

--- iProjectTeacher/Calendar&Schedule/TimeSchaduleInput.swift
+++ iProjectTeacher/Calendar&Schedule/TimeSchaduleInput.swift
@@ -34,23 +34,6 @@ class TimeScaduleInput{
     
     init(_ timeNum: Int, _ teacherStatus: String, _ studentStatus: String){
         
-        self.timeText = timeNum.s + ":00 - " + (timeNum + 1).s + ":00"
-        self.timeNum = timeNum
-        self.status = margeStatus(teacherStatus, studentStatus)
-        
-        switch status {
-        case "selected":
-            self.statusColor = UIColor(red: 1.f, green: 0.f, blue: 0.f, alpha: 1.f)
-        case "favorite":
-            self.statusColor = UIColor(red: 0.4.f, green: 0.8.f, blue: 1.f, alpha: 1.f)
-        case "onlyTeacherFavorite":
-            self.statusColor = UIColor(red: 0.5.f, green: 1.f, blue: 0.5.f, alpha: 1.f)
-        case "onlyStuderntFavorite":
-            self.statusColor = UIColor(red: 0.5.f, green: 1.f, blue: 0.5.f, alpha: 1.f)
-        default:
-            self.statusColor = UIColor(red: 1.f, green: 1.f, blue: 0.5.f, alpha: 1.f)
-        }
-        
         func margeStatus(_ teacherStatus: String, _ studentStatus: String) -> String {
             if teacherStatus == "selected" {
                 return "selected"
@@ -69,5 +52,24 @@ class TimeScaduleInput{
             }
             return ""
         }
+        
+        self.timeText = timeNum.s + ":00 - " + (timeNum + 1).s + ":00"
+        self.timeNum = timeNum
+        self.status = margeStatus(teacherStatus, studentStatus)
+        
+        switch status {
+        case "selected":
+            self.statusColor = UIColor(red: 1.f, green: 0.f, blue: 0.f, alpha: 1.f)
+        case "favorite":
+            self.statusColor = UIColor(red: 0.4.f, green: 0.8.f, blue: 1.f, alpha: 1.f)
+        case "onlyTeacherFavorite":
+            self.statusColor = UIColor(red: 0.5.f, green: 1.f, blue: 0.5.f, alpha: 1.f)
+        case "onlyStuderntFavorite":
+            self.statusColor = UIColor(red: 0.5.f, green: 1.f, blue: 0.5.f, alpha: 1.f)
+        default:
+            self.statusColor = UIColor(red: 1.f, green: 1.f, blue: 0.5.f, alpha: 1.f)
+        }
+
+
     }
 }
